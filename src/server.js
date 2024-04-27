af36e0f4eff12fe4202b9c51a7c4fd497c686090
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2024-04-27 17:40:08 -0400

    Baseline 1.17
    Update to include server data

---
+++ src/server.js
@@ -0,0 +1,411 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const fetch = require('node-fetch'); 
+require('dotenv').config();
+
+const app = express();
+const printifyApiKey = process.env.PRINTIFY_API_KEY
+const printifyShopID = process.env.PRINTIFY_SHOPID
+const PORT = process.env.PORT || 5000; 
+const mapAPIkey = process.env.MAP_API
+const paypalkey = process.env.PAYPAL_CLIENT_ID_SB
+const paypal = require('paypal-rest-sdk');
+
+const publishProduct = async (productId) => {
+    try {
+        const publishResponse = await fetch(`https://api.printify.com/v1/shops/${printifyShopID}/products/${productId}/publish.json`, {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${printifyApiKey}`, 
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify({
+          title: true,
+          description: true,
+          images: true,
+          variants: true,
+          tags: true,
+          keyFeatures: true,
+          shipping_template: true,
+        }),
+      });
+  
+      if (publishResponse.ok) {
+        console.log(`Product ${productId} published successfully.`);
+      } else {
+        console.error(`Failed to publish product ${productId}.`);
+      }
+    } catch (error) {
+      console.error('Error in publishing product:', error);
+    }
+};
+
+// Error handling middleware
+const errorHandler = (err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).json({ error: 'Internal Server Error my guy', details: err.message });
+};
+
+const apiProxy = createProxyMiddleware('/products', {
+  target: `https://api.printify.com/v1/shops/${printifyShopID}`,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/products': '/products.json',
+  },
+  onProxyReq: function (proxyReq) {
+    proxyReq.setHeader('Authorization', `Bearer ${printifyApiKey}`);
+  },
+});
+
+const orderProxy = createProxyMiddleware('/orders', {
+  target: `https://api.printify.com/v1/shops/${printifyShopID}`,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/orders': '/orders.json',
+  },
+  onProxyReq: function (proxyReq) {
+    proxyReq.setHeader('Authorization', `Bearer ${printifyApiKey}`);
+  },
+});
+
+const mapProxy = createProxyMiddleware('/maps/regions', {
+  target: 'https://api.countrystatecity.in/v1/countries/${selectedCountry}/states`',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/maps/regions': '/maps/regions.json',
+  },
+  onProxyReq: function (proxyReq) {
+    proxyReq.setHeader('Authorization', `Bearer ${mapAPIkey}`);
+  },
+});
+
+const mapcitiesProxy = createProxyMiddleware('/maps/cities', {
+  target: 'https://api.countrystatecity.in/v1/countries/${selectedCountry}/states`',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/maps/cities': '/maps/cities.json',
+  },
+  onProxyReq: function (proxyReq) {
+    proxyReq.setHeader('Authorization', `Bearer ${mapAPIkey}`);
+  },
+});
+
+const paypayProxy = createProxyMiddleware('/config', {
+  target: `https://www.sandbox.paypal.com/sdk/js?client-id=${paypalkey}`,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/config': '/config.json',
+  },
+  // onProxyReq: function (proxyReq) {
+  //   proxyReq.setHeader('Authorization', `Bearer ${paypalkey}`);
+  // },
+});
+
+const paypayProxy2 = createProxyMiddleware('/validate', {
+  target: `https://www.sandbox.paypal.com/sdk/js?client-id=${paypalkey}`,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/validate': '/validate.json',
+  },
+  onProxyReq: function (proxyReq) {
+    proxyReq.setHeader('Authorization', `Bearer ${paypalkey}`);
+  },
+});
+
+app.use((req, res, next) => {
+    let access = '';
+  
+    // Check the origin of the request
+    if (req.headers.origin === 'http://127.0.0.1:5500') {
+      access = 'http://127.0.0.1:5500';
+    } 
+    else if (req.headers.origin === 'https://paul-matthew.github.io') {
+      access = 'https://paul-matthew.github.io';
+    }
+    else if (req.headers.origin === 'https://www.tropicalmisfit.com') {
+      access = 'https://www.tropicalmisfit.com';
+    }
+    
+    // Set the appropriate Access-Control-Allow-Origin header
+    res.header('Access-Control-Allow-Origin', access);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
+//Code to prevent clickjacking - recommendation from penetration testing
+app.use((req, res, next) => {
+  res.header('X-Frame-Options', 'DENY');
+  next();
+});
+
+// Set Content Security Policy header to allow scripts only from the same origin - recommendation from penetration testing
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "script-src 'self'");
+  next();
+});
+
+// Set HSTS header with a max-age of 1 year - recommendation from penetration testing
+app.use((req, res, next) => {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  next();
+});
+
+// Set X-Frame-Options header to deny framing - recommendation from penetration testing
+app.use((req, res, next) => {
+  res.setHeader('X-Frame-Options', 'DENY');
+  next();
+});
+
+
+
+
+
+//PRODUCTS
+
+app.get('/fetch-and-publish-products', async (req, res) => {
+  try {
+    const printifyResponse = await fetch(`https://api.printify.com/v1/shops/${printifyShopID}/products.json`, {
+      headers: {
+        Authorization: `Bearer ${printifyApiKey}`,
+      }
+    });
+
+    if (printifyResponse.ok) {
+      const products = await printifyResponse.json();
+
+      // Publish products retrieved from Printify
+      for (const product of products) {
+        const productId = product.id;
+        await publishProduct(productId);
+      }
+
+      res.status(200).json({ message: 'Products fetched and published successfully' });
+    } else {
+      res.status(500).json({ error: 'Failed to fetch products from Printify' });
+    }
+  } catch (error) {
+    // Handle any errors that occur during order processing
+    console.error('Error processing order:', error);
+  
+    // Log additional details
+    console.error('Request body:');
+  
+    // Send a detailed error response
+    res.status(500).json({ error: 'Failed to process order', details: error.message });
+  }  
+});
+
+
+app.use(express.json());
+
+//Order
+
+app.post('/orders', async (req, res) => {
+  const {address_to } = req.body;
+
+  if (!address_to) {
+    console.log("this is the address that was given:", address_to);
+    return res.status(400).json({ error: 'Invalid address data' });
+  }
+
+  const {line_items} = req.body;
+
+  if (!line_items) {
+    console.log("This is the product info for the order", line_items)
+    return res.status(400).json({ error: 'Invalid order/shipping data' });
+  }
+
+  try {
+    console.log('Request Body:', req.body);
+
+    // const clientLineItems = req.body.lineItems || [];
+
+    // const serverLineItems = clientLineItems.map(item => ({
+    //   product_id: item.product_id,
+    //   variant_id: item.variant_id,
+    //   quantity: item.quantity,
+    // }));
+
+    const orderResponse = await fetch(`https://api.printify.com/v1/shops/${printifyShopID}/orders.json`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${printifyApiKey}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    console.log('Printify API Response:', orderResponse.status, orderResponse.statusText);
+
+    if (orderResponse.ok) {
+      console.log('Order placed successfully with Printify.');
+      res.status(200).json({   
+        success: true,
+        orderStatus: orderResponse.statusText,
+        message: 'Order placed successfully with Printify', 
+      });
+    } else {
+      console.error('Failed to place order with Printify.');
+      res.status(500).json({   
+        success: false,
+        orderStatus: orderResponse.statusText,
+        error: 'Failed to place order with Printify', 
+      });
+    }
+  } catch (error) {
+    console.error('Error processing order:', error);
+    res.status(500).json({ error: 'Failed to process order' });
+  }
+});
+
+
+//MAP/REGIONS
+
+app.get('/maps/regions', async (req, res) => {
+  const selectedCountry = req.query.country;
+
+  const apiUrl = `https://api.countrystatecity.in/v1/countries/${selectedCountry}/states`;
+  const headers = new Headers();
+  headers.append("X-CSCAPI-KEY", mapAPIkey);
+
+  const requestOptions = {
+    method: 'GET',
+    headers: headers,
+    redirect: 'follow'
+  };
+
+  try {
+    const response = await fetch(apiUrl, requestOptions);
+    const result = await response.json();
+
+    // Process the result based on the selected country
+    switch (selectedCountry) {
+
+
+
+      default:
+        res.json(result);
+        break;
+    }
+  } catch (error) {
+    console.log('Error fetching states:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//MAP/CITIES
+
+app.get('/maps/cities', async (req, res) => {
+  const selectedCountry = req.query.country;
+  const selectedRegion = req.query.region;
+
+  const apiUrl = `https://api.countrystatecity.in/v1/countries/${selectedCountry}/states/${encodeURIComponent(selectedRegion)}/cities`;
+  const headers = new Headers();
+  headers.append("X-CSCAPI-KEY", mapAPIkey);
+
+  const requestOptions = {
+    method: 'GET',
+    headers: headers,
+    redirect: 'follow'
+  };
+
+  try {
+    const response = await fetch(apiUrl, requestOptions);
+    const result = await response.json();
+
+    // Process the result based on the selected country
+    switch (selectedCountry) {
+      // Add cases for specific countries if needed
+
+      default:
+        res.json(result);
+        break;
+    }
+  } catch (error) {
+    console.log('Error fetching cities:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//Paypal
+
+paypal.configure({
+  mode: 'sandbox', // Change to 'live' for production
+  client_id: process.env.PAYPAL_CLIENT_ID_SB,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET_SB,
+  // Add other configuration options as needed
+});
+
+app.get('/config', (req, res) => {
+  res.json({
+    paypalClientId: process.env.PAYPAL_CLIENT_ID_SB,
+    silversurfer:"In the fresh",
+  });
+});
+
+// New route for validating a PayPal payment
+app.post('/validate', async (req, res) => {
+  const {paymentDetails,total } = req.body;
+
+  if (!paymentDetails) {
+    return res.status(400).json({ error: 'Invalid request data' });
+  }
+
+  try {
+
+    // Check if the payment details match the order details
+    const isPaymentValid = validatePaymentDetails(paymentDetails, total);
+    // const isPaymentValid = 1;
+
+
+    if (isPaymentValid) {
+      // If the payment is valid, send a success response to the client
+      res.json({ success: true });
+    } else {
+      // If the payment is not valid, send an error response to the client
+      res.status(400).json({ success: false, error: 'Invalid payment' });
+    }
+  } catch (error) {
+    console.error('Error processing PayPal order:', error);
+    res.status(500).json({ success: false, error: 'Failed to process PayPal order' });
+  }
+});
+
+
+
+// Function to validate payment details
+function validatePaymentDetails(paymentDetails,total) {
+  const isValid =
+    paymentDetails &&
+    paymentDetails.status === 'COMPLETED' &&
+    paymentDetails.purchase_units &&
+    paymentDetails.purchase_units[0] &&
+    paymentDetails.purchase_units[0].amount &&
+    paymentDetails.purchase_units[0].amount.value === total.toString();
+    console.log("Paypal platform total",paymentDetails.purchase_units[0].amount.value)
+    console.log("client side total",total.toString());
+
+    if (isValid) {
+      console.log('Server validation complete');
+    } else {
+      console.log('Server validation error');
+      console.log('paymentDetails:', paymentDetails);
+    }
+
+  return isValid;
+}
+
+
+app.use(apiProxy);
+app.use(orderProxy);
+app.use(blogProxy);
+app.use(mapProxy);
+app.use(mapcitiesProxy);
+app.use(paypayProxy);
+app.use(paypayProxy2);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Proxy server running on PORT ${PORT}`);
+});
---
+++ src/shop.js
@@ -0,0 +1,194 @@
+import React, { useState } from 'react';
+import './App.css';
+import App from './App'
+import Blog from './deals'
+import Contact from './contact'
+import Modal from './modal'
+// import ServicesMobile from './components/ServicesMobile';
+import FadeInSection from './components/FadeIn';
+import ReactDOM from 'react-dom';
+
+
+function Services() {
+    const [desktopState] = useState('Services1');  
+    const [showModal, setShowModal] = useState(false);
+    const [selectedService, setSelectedService] = useState(null);
+    //   const handleContactClick = () => {
+    //     setDesktopState('Contact');
+    //   };
+
+
+
+      const services = [
+        { 
+            title: "Product#1", 
+            description: <p><strong>Price:</strong> $XX.XX</p>,            
+            image:'./empress-tshirt.png',
+            description2: (
+                <div>
+                    <p>[Product info Placeholder.  Popup to be further developed.]</p>
+                    {/* <p><strong>Key Features:</strong></p>
+                    <ul>
+                        <li>State Analysis: Thorough assessment of your organization's operational, financial, and strategic performance.</li>
+                        <li>SWOT Analysis: Identification of strengths, weaknesses, opportunities, and threats impacting your business.</li>
+                        <li>Strategic Recommendations: Data-driven recommendations and actionable insights to optimize business processes and enhance performance.</li>
+                    </ul> */}
+                </div>
+            )
+        },
+        { 
+            title: "Product#2", 
+            description: <p><strong>Price:</strong> $XX.XX</p>, 
+            image:"./empress-tshirt.png",
+            description2: (
+                <div>
+                    <p>[Product info Placeholder]</p>
+                    {/* <p><strong>Key Features:</strong></p>
+                    <ul>
+                        <li>Operational Audits: Evaluation of your organization's day-to-day operations and processes.</li>
+                        <li>Financial Audits: Review of financial statements, budgets, and financial controls.</li>
+                        <li>Compliance Audits: Assessment of compliance with industry regulations and standards.</li>
+                    </ul> */}
+                </div>
+            )
+        },
+        { 
+            title: "Product#3", 
+            description: <p><strong>Price:</strong> $XX.XX</p>, 
+            image:"./empress-tshirt.png",
+            description2: (
+                <div>
+                    <p>[Product info Placeholder]</p>
+                    {/* <p><strong>Key Features:</strong></p>
+                    <ul>
+                        <li>Performance Measurement: Evaluation of program performance against predefined objectives and benchmarks.</li>
+                        <li>Outcome Assessment: Analysis of program outcomes and impact on target beneficiaries or stakeholders.</li>
+                        <li>Recommendation Development: Identification of strategies and recommendations for program improvement and optimization.</li>
+                    </ul> */}
+                </div>
+            )
+        },
+        { 
+            title: "Product#4", 
+            description: <p><strong>Price:</strong> $XX.XX</p>, 
+            image:"./empress-tshirt.png",
+            description2: (
+                <div>
+                    <p>[Product info Placeholder]</p>
+                    {/* <p><strong>Key Components:</strong></p>
+                    <ul>
+                        <li>Vision and Mission: Defining the organization's purpose, values, and long-term aspirations.</li>
+                        <li>Strategic Goals: Identifying high-level strategic objectives and priorities to guide decision-making.</li>
+                        <li>Action Plans: Developing detailed action plans with specific initiatives, timelines, and responsible parties.</li>
+                    </ul> */}
+                </div>
+            )
+        },
+    ];
+    
+
+    const handleReadMoreClick = (service) => {
+        setSelectedService(service);
+        setShowModal(true);
+        document.body.classList.add('modal-open');
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setSelectedService(null);
+        document.body.classList.remove('modal-open');
+    };
+  
+  return (
+    <>
+    <div className="Services" style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px',background: 'white'}}>
+        <div id='NavlogoMobile'><img src='./logo2.png' alt='logo mobile'style={{height:'6vh',marginTop:'1vh', position:'fixed', left:10, top:10}}onClick={() => window.location.href = './'}/></div>
+        <div className='Banner'><img src='./banner3.png' alt='banner'style={{zIndex:2,width:'100vw',maxHeight:'200px',marginTop:'3vh', left:0, top:0,objectFit:'cover'}}onClick={() => window.location.href = './'}/></div>
+        {desktopState === 'Services1' && (
+            <div className="Servicesx" style={{width: '100%', position: 'relative', background: 'white',top: '100px',marginTop: '-90px',flexDirection: 'column', display: 'inline-flex',justifyContent: 'flex-start', alignItems: 'center'}}>
+                <FadeInSection>
+                    <div style={{ backgroundColor:'white' }}>
+                        <div className="MySkill" style={{ height: "auto", flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', gap: 56.67, display: 'inline-flex', padding: "0px 4vw" }}>
+                            <div className="SectionTitle" style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', gap: '1vh', display: 'flex' }}>
+                                <div className="Content" style={{ alignSelf: 'stretch', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 32, display: 'flex' }}>
+                                    <div className="MyExpertise" style={{ alignSelf: 'stretch', color: '#282938', fontSize: 'clamp(40px, 4vw, 61px)', fontFamily: 'PlayfairDisplay', fontWeight: '700', wordWrap: 'break-word' }}>Shop</div>
+                                </div>
+                                <div className="MyExpertise" style={{ alignSelf: 'stretch', color: '#282938', fontSize: 'clamp(20px, 4vw, 20px)', fontFamily: 'PlayfairDisplay', fontWeight: '500', wordWrap: 'break-word' }}>“<i>I’m so excited to share with you my range of products that I have personally designed. I hope they bring a smile to your day.</i>” - Dr. Joi</div>
+                                <div className="MyExpertise" style={{ alignSelf: 'stretch', color: 'red', fontSize: 'clamp(30px, 4vw, 61px)', fontFamily: 'PlayfairDisplay', fontWeight: '700', wordWrap: 'break-word',marginTop:'30px' }}>Limited Time Offers</div>
+                                {/* <div className="MyExpertise" style={{ alignSelf: 'stretch', color: '#282938', fontSize: 'clamp(20px, 4vw, 20px)', fontFamily: 'PlayfairDisplay', fontWeight: '500', wordWrap: 'break-word' }}>Discover products available for a limited time only. Don't miss out – grab yours today!</div> */}
+                            </div>
+                        </div>
+                    </div>
+                </FadeInSection>
+                <div className="BodyX" style={{maxWidth:'100%',flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 20, display: 'inline-flex', marginTop:'50px'}}>
+                {/* THIS IS FOR THE PRODUCTS JS */}
+                    <FadeInSection>
+                        <div id="products-container" class="row justify-content-center" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000"></div> 
+                        <div className="Frame2230" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+                            {services.map((service, index) => (
+                            <div className="Servicecard" key={index} style={{ position: 'relative', backgroundColor: 'white', border: 'black 2px solid', width: '250px', margin: '20px', padding: '10px' }}>
+                                <div className="CardTitle">
+                                    <div className="TitleSer">{service.title}</div>
+                                </div>
+                                <div className="CardImage" onClick={() => handleReadMoreClick(service)}>
+                                    <img className="cardpic" style={{ maxHeight: '200px', opacity: '80%' }} src={`./${service.image}`} alt={service.title} />
+                                </div>
+                                <div className="CardTextx" style={{ paddingLeft: '20px', paddingRight: '30px' }}>{service.description}</div>
+                            </div>
+                            ))}
+                        </div>
+                    </FadeInSection>
+
+                    <FadeInSection>
+                        <div className="MyExpertise" style={{ alignSelf: 'stretch', color: 'black', fontSize: 'clamp(30px, 4vw, 51px)', fontFamily: 'PlayfairDisplay', fontWeight: '700', wordWrap: 'break-word',marginTop:'30px' }}>Our Full Collection</div>
+                        <div className="Frame2230" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center',marginBottom:'100px' }}>
+                            {services.map((service, index) => (
+                                <div className="Servicecard" key={index} style={{ position: 'relative', backgroundColor: 'white', border: 'black 2px solid', width: '250px', margin: '20px', padding: '10px' }}>
+                                    <div className="CardTitle">
+                                        <div className="TitleSer">{service.title}</div>
+                                    </div>
+                                    <div className="CardImage" onClick={() => handleReadMoreClick(service)}>
+                                        <img className="cardpic" style={{ maxHeight: '200px', opacity: '80%' }} src={`./${service.image}`} alt={service.title} />
+                                    </div>
+                                    <div className="CardTextx" style={{ paddingLeft: '20px', paddingRight: '30px' }}>{service.description}</div>
+                                </div>
+                            ))}
+                        </div>
+                    </FadeInSection>
+                </div>
+                {showModal && <Modal onClose={handleCloseModal} service={selectedService} />}
+                {showModal ? <div className="background-overlay" /> : null}
+            </div>
+        )}
+        {desktopState === 'Homedesktop1' && (
+        <App />
+        )}
+        {desktopState === 'Blog' && (
+        <Blog />
+        )}
+        {desktopState === 'Contact' && (
+        <Contact />
+        )}
+
+        {/* <div className="Navmenudesktop" style={{width: '80vw', height: '10vh', right: '0vw', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', display: 'inline-flex'}}>
+            <div className="ListitemDesktopNa" style={{width: 115, height: '100%', paddingLeft: 18, paddingRight: 18, paddingTop: 0, paddingBottom: 0, background: '#0E0E0E', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+                <div className="Label" style={{width: 100, textAlign: 'center', color: 'white', fontSize: 'clamp(13px, 2vw, 16px)', fontFamily: 'Roboto', fontWeight: '500', letterSpacing: 2, wordWrap: 'break-word'}}onClick={() => handleAboutClick()}>About</div>
+            </div>
+            <div className="ListitemDesktopNaActive" style={{zIndex:2,width: 115, height: '100%', paddingLeft: 38, paddingRight: 38, paddingTop: 0, paddingBottom: 0, background: 'rgb(130, 117, 78)', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+                <div className="Label" style={{width: 131, textAlign: 'center', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)',fontSize: 'clamp(13px, 2vw, 16px)', fontFamily: 'Roboto', fontWeight: '500', letterSpacing: 2, wordWrap: 'break-word'}}onClick={() => handleServicesClick()}>Services</div>
+            </div>
+            <div className="ListitemDesktopNa" style={{width: 115, height: '100%', paddingLeft: 38, paddingRight: 38, paddingTop: 0, paddingBottom: 0, background: '#0E0E0E', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+                <div className="Label" style={{width: 131, textAlign: 'center', color: 'white', fontSize: 'clamp(13px, 2vw, 16px)', fontFamily: 'Roboto', fontWeight: '500', letterSpacing: 2, wordWrap: 'break-word'}}onClick={() => handleBlogClick()}>Blog</div>
+            </div>
+            <div className="ListitemDesktopNa" style={{width: 115, height: '100%', paddingLeft: 38, paddingRight: 38, paddingTop: 0, paddingBottom: 0, background: '#0E0E0E', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+                <div className="Label" style={{width: 131, textAlign: 'center', color: 'white', fontSize: 'clamp(13px, 2vw, 16px)', fontFamily: 'Roboto', fontWeight: '500', letterSpacing: 2, wordWrap: 'break-word'}}onClick={() => handleContactClick()}>Contact</div>
+            </div>
+        </div> */}
+    </div>
+    {/* <ServicesMobile /> */}
+    </>
+  );
+}
+
+ReactDOM.render(<Services />, document.getElementById('root'));
+export default Services;
