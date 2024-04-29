b90500257fcba72bff8aa720432f221f322709ba
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2024-04-29 01:29:37 -0400

    Baseline 1.30
    debug of url attempt

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
+import About from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <About />
   </React.StrictMode>
 );
 
