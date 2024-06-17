11d95190917a2921e80bbddb421174f9cc6de937
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2024-06-16 20:33:34 -0400

    Baseline 1.75
    Setmore Booking button added

--- src/lovelab.js
+++ src/lovelab.js
@@ -33,8 +33,10 @@ const LoveLab = () => {
             <FadeInSection>
             <div className="section">
                 <div className="sectionText">
-                <h2>Live Podcasts</h2>
-                <p>8pm Wednesdays – Live Podcasts</p>
+                    <h2>Live Podcasts</h2>
+                    <p>8pm Wednesdays – Live Podcasts</p>
+                    <p>Join me every Wednesday at 8pm for engaging live podcasts where we explore topics on dating, relationships, and personal growth. Participate in real-time discussions, ask questions, and share your experiences in a supportive community.</p>
+                    <p>These sessions provide practical advice and insights to help you improve your relationships and personal development. Don't miss the chance to connect with like-minded individuals and gain valuable knowledge every Wednesday evening.</p>                
                 </div>
                 <img src="./home2.png" alt="Live Podcasts" className="sectionImage" />
             </div>
@@ -43,17 +45,14 @@ const LoveLab = () => {
             <div className="section">
                 <img src="./home3.png" alt="Personalized Coaching" className="sectionImage" />
                 <div className="sectionText">
-                <h2>Personalized Coaching</h2>
-                <p>Offer Personalized Coaching:</p>
-                <ul class='coachingbull'>
-                    <li>Offering one-on-one coaching sessions for viewers who desire more personalized guidance and support in their dating journey.</li>
-                    <li>Set clear pricing and availability for coaching sessions, and promote them during my live sessions and on my social media platforms.</li>
-                </ul>
+                    <h2>Personalized Coaching</h2>
+                    <p>Offering one-on-one coaching sessions to provide you with personalized guidance and support in your dating journey. Book an appointment today to start making positive changes.</p>
                 </div>
-          </div>
+            </div>
         </FadeInSection>
       </div>
       </div>
+      <a style={{float:'none', position: 'fixed', right: '-2px', top: '25%', display: 'block', zIndex: '20000'}}rel="noreferrer" target="_blank"  href="https://booking.setmore.com/scheduleappointment/d1a338ee-1d7d-447f-aaf7-9937ed4774ac"><img border="none" src="https://storage.googleapis.com/full-assets/setmore/images/1.0/Calendar/Setmore-Book-Now.png" alt="Book an appointment with Paul using Setmore"/></a>
     </div>
   );
 };
