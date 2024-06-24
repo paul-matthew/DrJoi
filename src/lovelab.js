840b3b009617359b6543e6ed8e9acb0cc9b5b3ce
irvan saharudin <irvansaharudin@gmail.com>
2024-06-24 23:01:07 +0700

    fix horizontal scrolling

--- src/lovelab.js
+++ src/lovelab.js
@@ -1,61 +1,158 @@
-import React from 'react';
-import FadeInSection from './components/FadeIn.js';
-import './App.css';
+import React from "react";
+import FadeInSection from "./components/FadeIn.js";
+import "./App.css";
 
 const LoveLab = () => {
   return (
-    <div className="BlogContainer">
-      <div id='NavlogoMobile'>
-        <img src='./logo-green3.png' alt='logo mobile' onClick={() => window.location.href = './'} />
+    <div
+      className="BlogContainer"
+      style={{
+        maxWidth: "100vw",
+        overflow: "hidden",
+      }}
+    >
+      <div id="NavlogoMobile">
+        <img
+          src="./logo-green3.png"
+          alt="logo mobile"
+          onClick={() => (window.location.href = "./")}
+        />
       </div>
-      <div className='Banner'>
-        <img src='./bannerflower.png' alt='banner' />
+      <div className="Banner">
+        <img src="./bannerflower.png" alt="banner" />
       </div>
       <div className="contentContainer">
-        <div className="MySkill" style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', gap: 56.67, display: 'inline-flex', padding: "0px 3vw" }}>
+        <div
+          className="MySkill"
+          style={{
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            gap: 56.67,
+            display: "inline-flex",
+            padding: "0px 3vw",
+          }}
+        >
+          <div
+            className="SectionTitle"
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "flex-start",
+              gap: "3vh",
+              display: "flex",
+            }}
+          >
+            <div
+              className="Content"
+              style={{
+                alignSelf: "stretch",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+                gap: 32,
+                display: "flex",
+              }}
+            >
+              <div
+                className="MyExpertise"
+                style={{
+                  alignSelf: "stretch",
+                  color: "#282938",
+                  fontSize: "clamp(40px, 4vw, 61px)",
+                  fontFamily: "PlayfairDisplay",
+                  fontWeight: "700",
+                  wordWrap: "break-word",
+                }}
+              >
+                Love Lab
+              </div>
+            </div>
+          </div>
 
-        <div className="SectionTitle" style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', gap: '3vh', display: 'flex' }}>
-
-        <div className="Content" style={{ alignSelf: 'stretch', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 32, display: 'flex' }}>
-            <div className="MyExpertise" style={{ alignSelf: 'stretch', color: '#282938', fontSize: 'clamp(40px, 4vw, 61px)', fontFamily: 'PlayfairDisplay', fontWeight: '700', wordWrap: 'break-word' }}>Love Lab</div>
-        </div>
-        </div>
-
-            <FadeInSection>
+          <FadeInSection>
             <div className="section">
-                <img src="./home1c.png" alt="Personal Guidance" className="sectionImage" />
-                <div className="sectionText">
+              <img
+                src="./home1c.png"
+                alt="Personal Guidance"
+                className="sectionImage"
+              />
+              <div className="sectionText">
                 <h2>Personal Guidance</h2>
-                <p>By leveraging my positive and supportive persona, I want to help men navigate the complexities of dating and relationships by providing valuable insights and guidance while encouraging a sense of empowerment and self-improvement among my audience.</p>
-                </div>
+                <p>
+                  By leveraging my positive and supportive persona, I want to
+                  help men navigate the complexities of dating and relationships
+                  by providing valuable insights and guidance while encouraging
+                  a sense of empowerment and self-improvement among my audience.
+                </p>
+              </div>
             </div>
-            </FadeInSection>
-            <FadeInSection>
+          </FadeInSection>
+          <FadeInSection>
             <div className="section">
-                <div className="sectionText">
-                    <h2>Live Podcasts</h2>
-                    <p>8pm Wednesdays – Live Podcasts</p>
-                    <p>Join me every Wednesday at 8pm for engaging live podcasts where we explore topics on dating, relationships, and personal growth. Participate in real-time discussions, ask questions, and share your experiences in a supportive community.</p>
-                    <p>These sessions provide practical advice and insights to help you improve your relationships and personal development. Don't miss the chance to connect with like-minded individuals and gain valuable knowledge every Wednesday evening.</p>                
-                </div>
-                <img src="./home2.png" alt="Live Podcasts" className="sectionImage" />
+              <div className="sectionText">
+                <h2>Live Podcasts</h2>
+                <p>8pm Wednesdays – Live Podcasts</p>
+                <p>
+                  Join me every Wednesday at 8pm for engaging live podcasts
+                  where we explore topics on dating, relationships, and personal
+                  growth. Participate in real-time discussions, ask questions,
+                  and share your experiences in a supportive community.
+                </p>
+                <p>
+                  These sessions provide practical advice and insights to help
+                  you improve your relationships and personal development. Don't
+                  miss the chance to connect with like-minded individuals and
+                  gain valuable knowledge every Wednesday evening.
+                </p>
+              </div>
+              <img
+                src="./home2.png"
+                alt="Live Podcasts"
+                className="sectionImage"
+              />
             </div>
-            </FadeInSection>
-            <FadeInSection>
+          </FadeInSection>
+          <FadeInSection>
             <div className="section">
-                <img src="./home3.png" alt="Personalized Coaching" className="sectionImage" />
-                <div className="sectionText">
-                    <h2>Personalized Coaching</h2>
-                    <p>Offering one-on-one coaching sessions to provide you with personalized guidance and support in your dating journey. Book an appointment today to start making positive changes.</p>
-                </div>
+              <img
+                src="./home3.png"
+                alt="Personalized Coaching"
+                className="sectionImage"
+              />
+              <div className="sectionText">
+                <h2>Personalized Coaching</h2>
+                <p>
+                  Offering one-on-one coaching sessions to provide you with
+                  personalized guidance and support in your dating journey. Book
+                  an appointment today to start making positive changes.
+                </p>
+              </div>
             </div>
-        </FadeInSection>
+          </FadeInSection>
+        </div>
       </div>
-      </div>
-      <a style={{float:'none', position: 'fixed', right: '-2px', top: '25%', display: 'block', zIndex: '20000'}}rel="noreferrer" target="_blank"  href="https://booking.setmore.com/scheduleappointment/d1a338ee-1d7d-447f-aaf7-9937ed4774ac"><img border="none" src="https://storage.googleapis.com/full-assets/setmore/images/1.0/Calendar/Setmore-Book-Now.png" alt="Book an appointment with Paul using Setmore"/></a>
+      <a
+        style={{
+          float: "none",
+          position: "fixed",
+          right: "-2px",
+          top: "25%",
+          display: "block",
+          zIndex: "20000",
+        }}
+        rel="noreferrer"
+        target="_blank"
+        href="https://booking.setmore.com/scheduleappointment/d1a338ee-1d7d-447f-aaf7-9937ed4774ac"
+      >
+        <img
+          border="none"
+          src="https://storage.googleapis.com/full-assets/setmore/images/1.0/Calendar/Setmore-Book-Now.png"
+          alt="Book an appointment with Paul using Setmore"
+        />
+      </a>
     </div>
   );
 };
 
 export default LoveLab;
-
--- src/wellness.js
+++ src/wellness.js
@@ -1,85 +1,203 @@
-import './App.css';
-import FadeInSection from './components/FadeIn.js';
-import React from 'react';
+import "./App.css";
+import FadeInSection from "./components/FadeIn.js";
+import React from "react";
 
 const Wellness = () => {
-    return (
-        <div className="BlogContainer" style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center',padding: '20px',background: 'white' }}>
-            <div id='NavlogoMobile'><img src='./logo-green3.png' alt='logo mobile'style={{height:'7vh',marginTop:'0vh', position:'fixed', left:10, top:10}}onClick={() => window.location.href = './'}/></div>
-            <div className='Banner'><img src='./bannerflower.png' alt='banner'style={{zIndex:2,width:'100vw',maxHeight:'200px',marginTop:'3vh', objectFit:'cover'}}/></div>
-            <div className="PostsWrapper" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px',marginTop: '30px' }}>
-                <FadeInSection>
-                <div style={{ backgroundColor:'white' }}>
-                    <div className="MySkill" style={{ height: "auto", flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', gap: 56.67, display: 'inline-flex', padding: "0px 3vw" }}>
-                        <div className="SectionTitle" style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', gap: '1vh', display: 'flex' }}>
-                            <div className="Content" style={{ alignSelf: 'stretch', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 32, display: 'flex' }}>
-                                <div className="MyExpertise" style={{ alignSelf: 'stretch', color: '#282938', fontSize: 'clamp(40px, 4vw, 61px)', fontFamily: 'PlayfairDisplay', fontWeight: '700', wordWrap: 'break-word' }}>Spiritual Wellness Hub</div>
-                            </div>
-                            <div className="MyExpertise" style={{ alignSelf: 'stretch', color: '#282938', fontSize: 'clamp(15px, 4vw, 25px)', fontFamily: 'PlayfairDisplay', fontWeight: '500', wordWrap: 'break-word' }}>“<i>This is the content that I personally use to nurture my spiritual well-being and enhance my daily journey towards inner peace and fulfillment. Dive in and explore these carefully curated resources designed to uplift your spirit and elevate your consciousness.</i>” - Dr. Joi</div>
-                        </div>
-                        <FadeInSection>
-                        <div className="Row">
-                            <div className="Video">
-                                <iframe
-                                width="560"
-                                height="315"
-                                src="https://www.youtube.com/embed/pIwzVer7ZDQ?si=-N_UpIM6I3VpDMt_"
-                                title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin"
-                                allowFullScreen
-                                style={{ border: "solid black", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)" }}
-                                ></iframe>
-                            </div>
-                            <div className="Video">
-                                <iframe
-                                width="560"
-                                height="315"
-                                src="https://www.youtube.com/embed/xdKglDO6Pd8?si=qOkoPeiJBjLplayD"
-                                title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin"
-                                allowFullScreen
-                                style={{ border: "solid black", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)" }}
-                                ></iframe>
-                            </div>
-                            <div className="Video">
-                                <iframe
-                                width="560"
-                                height="315"
-                                src="https://www.youtube.com/embed/UX5zqQuLMOw?si=Ma220d-we4cEbOjQ"
-                                title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin"
-                                allowFullScreen
-                                style={{ border: "solid black", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)" }}
-                                ></iframe>
-                            </div>
-                            <div className="Video">
-                                <iframe
-                                width="560"
-                                height="315"
-                                src="https://www.youtube.com/embed/3fi7DGQXgeU?si=o9qsEnW4DVzmZ9Zi"
-                                title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin"
-                                allowFullScreen
-                                style={{ border: "solid black", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)" }}
-                                ></iframe>
-                            </div>
-                        </div>
-                        </FadeInSection>
-                    </div>
+  return (
+    <div
+      className="BlogContainer"
+      style={{
+        maxWidth: "100vw",
+        overflow: "hidden",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "20px",
+        background: "white",
+      }}
+    >
+      <div id="NavlogoMobile">
+        <img
+          src="./logo-green3.png"
+          alt="logo mobile"
+          style={{
+            height: "7vh",
+            marginTop: "0vh",
+            position: "fixed",
+            left: 10,
+            top: 10,
+          }}
+          onClick={() => (window.location.href = "./")}
+        />
+      </div>
+      <div className="Banner">
+        <img
+          src="./bannerflower.png"
+          alt="banner"
+          style={{
+            zIndex: 2,
+            width: "100vw",
+            maxHeight: "200px",
+            marginTop: "3vh",
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <div
+        className="PostsWrapper"
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "20px",
+          marginTop: "30px",
+        }}
+      >
+        <FadeInSection>
+          <div style={{ backgroundColor: "white" }}>
+            <div
+              className="MySkill"
+              style={{
+                height: "auto",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                gap: 56.67,
+                display: "inline-flex",
+                padding: "0px 3vw",
+              }}
+            >
+              <div
+                className="SectionTitle"
+                style={{
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "flex-start",
+                  gap: "1vh",
+                  display: "flex",
+                }}
+              >
+                <div
+                  className="Content"
+                  style={{
+                    alignSelf: "stretch",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                    alignItems: "flex-start",
+                    gap: 32,
+                    display: "flex",
+                  }}
+                >
+                  <div
+                    className="MyExpertise"
+                    style={{
+                      alignSelf: "stretch",
+                      color: "#282938",
+                      fontSize: "clamp(40px, 4vw, 61px)",
+                      fontFamily: "PlayfairDisplay",
+                      fontWeight: "700",
+                      wordWrap: "break-word",
+                    }}
+                  >
+                    Spiritual Wellness Hub
+                  </div>
                 </div>
-                </FadeInSection>
+                <div
+                  className="MyExpertise"
+                  style={{
+                    alignSelf: "stretch",
+                    color: "#282938",
+                    fontSize: "clamp(15px, 4vw, 25px)",
+                    fontFamily: "PlayfairDisplay",
+                    fontWeight: "500",
+                    wordWrap: "break-word",
+                  }}
+                >
+                  “
+                  <i>
+                    This is the content that I personally use to nurture my
+                    spiritual well-being and enhance my daily journey towards
+                    inner peace and fulfillment. Dive in and explore these
+                    carefully curated resources designed to uplift your spirit
+                    and elevate your consciousness.
+                  </i>
+                  ” - Dr. Joi
+                </div>
+              </div>
+              <FadeInSection>
+                <div className="Row">
+                  <div className="Video">
+                    <iframe
+                      width="560"
+                      height="315"
+                      src="https://www.youtube.com/embed/pIwzVer7ZDQ?si=-N_UpIM6I3VpDMt_"
+                      title="YouTube video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                      allowFullScreen
+                      style={{
+                        border: "solid black",
+                        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",
+                      }}
+                    ></iframe>
+                  </div>
+                  <div className="Video">
+                    <iframe
+                      width="560"
+                      height="315"
+                      src="https://www.youtube.com/embed/xdKglDO6Pd8?si=qOkoPeiJBjLplayD"
+                      title="YouTube video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                      allowFullScreen
+                      style={{
+                        border: "solid black",
+                        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",
+                      }}
+                    ></iframe>
+                  </div>
+                  <div className="Video">
+                    <iframe
+                      width="560"
+                      height="315"
+                      src="https://www.youtube.com/embed/UX5zqQuLMOw?si=Ma220d-we4cEbOjQ"
+                      title="YouTube video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                      allowFullScreen
+                      style={{
+                        border: "solid black",
+                        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",
+                      }}
+                    ></iframe>
+                  </div>
+                  <div className="Video">
+                    <iframe
+                      width="560"
+                      height="315"
+                      src="https://www.youtube.com/embed/3fi7DGQXgeU?si=o9qsEnW4DVzmZ9Zi"
+                      title="YouTube video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                      allowFullScreen
+                      style={{
+                        border: "solid black",
+                        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",
+                      }}
+                    ></iframe>
+                  </div>
+                </div>
+              </FadeInSection>
             </div>
-        </div>
-    );
+          </div>
+        </FadeInSection>
+      </div>
+    </div>
+  );
 };
 
 export default Wellness;
-
