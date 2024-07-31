939f30803b71d60a721682574d334d955ccfe0fc
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2024-07-31 00:48:24 -0400

    Baseline 2.13
    update to contact page newsletter subscribe modal

--- src/contact.js
+++ src/contact.js
@@ -8,29 +8,30 @@ function Contact() {
     const [modalOpen, setModalOpen] = useState(false);
     const [subscribeEmail, setSubscribeEmail] = useState('');
     const [subscribeResponse, setSubscribeResponse] = useState('');
+    const [optIn, setOptIn] = useState(false);
+    const [privacyAccepted, setPrivacyAccepted] = useState(false);
     const modalRef = useRef(null);
-    // const [contactResponse, setContactResponse] = useState('');
 
     const smallScreen = clientWidth <= 640;
 
     useEffect(() => {
         const handleResize = () => {
-            setClientWidth(window.innerWidth)
-        }
+            setClientWidth(window.innerWidth);
+        };
 
-        window.addEventListener("resize", handleResize)
+        window.addEventListener("resize", handleResize);
 
         return () => {
-            window.removeEventListener("resize", handleResize)
-        }
-    }, [])
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (modalRef.current && !modalRef.current.contains(event.target)) {
                 setModalOpen(false);
             }
-        }
+        };
 
         if (modalOpen) {
             document.addEventListener("mousedown", handleClickOutside);
@@ -40,19 +41,21 @@ function Contact() {
 
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
-        }
+        };
     }, [modalOpen]);
 
     let fetchURL = "https://drjoiserver-106ea7a60e39.herokuapp.com/subscribe";
-    if (
-      window.location.hostname === "localhost" ||
-      window.location.hostname === "127.0.0.1"
-    ) {
-      fetchURL = "http://localhost:5000/subscribe";
+    if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+        fetchURL = "http://localhost:5000/subscribe";
     }
 
     const handleSubscribe = async (event) => {
         event.preventDefault();
+
+        if (!optIn || !privacyAccepted) {
+            setSubscribeResponse('You must agree to the terms and conditions.');
+            return;
+        }
 
         try {
             const response = await fetch(fetchURL, {
@@ -76,81 +79,73 @@ function Contact() {
     };
 
     let contactFetchURL = "https://drjoiserver-106ea7a60e39.herokuapp.com/contact";
-    if (
-      window.location.hostname === "localhost" ||
-      window.location.hostname === "127.0.0.1"
-    ) {
-      contactFetchURL = "http://localhost:5000/contact";
+    if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+        contactFetchURL = "http://localhost:5000/contact";
     }
-    
+
     const handleSubmit = async (event) => {
         event.preventDefault();
-        
+
         const formData = {
-          name: event.target.name.value,
-          email: event.target.email.value,
-          message: event.target.message.value,
+            name: event.target.name.value,
+            email: event.target.email.value,
+            message: event.target.message.value,
         };
-      
+
         try {
-          const response = await fetch(contactFetchURL, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-          });
-      
-          if (!response.ok) {
-            throw new Error('Failed to send message. Please try again.');
-          }
-      
-          const data = await response.json(); // Parse JSON response
-          console.log('Success:', data); // Log success message
-          alert('Email sent successfully!');
-          window.location.reload();           
-          // Handle success message display or any other logic
+            const response = await fetch(contactFetchURL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to send message. Please try again.');
+            }
+
+            const data = await response.json();
+            console.log('Success:', data);
+            alert('Email sent successfully!');
+            window.location.reload();
         } catch (error) {
-          console.error('Error:', error);
-          alert('An error occurred. Please try again.');        
+            console.error('Error:', error);
+            alert('An error occurred. Please try again.');
         }
-      };
-      
-            
+    };
 
     return (
-        <div className="ContactX" style={{ width: '100vw', right: '0', }}>
+        <div className="ContactX" style={{ width: '100vw', right: '0' }}>
             <div id='NavlogoMobile'>
                 <img src='./logo-green4.png' alt='logo mobile' style={{ height: '7vh', marginTop: '0vh' }} onClick={() => window.location.href = './'} />
             </div>
             {desktopState === 'Contact1' && (
                 <FadeInSection>
                     <div className="ContactX" style={{ width: '100%', top: "2vh", display: 'flex', justifyContent: 'center', alignItems: 'center', position: "relative" }}>
-                    <form className="ContactForm" style={{ width: '70%', minHeight: '30vh', border: 'solid gray', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }} onSubmit={handleSubmit}>
-                    <p className='contact-title' style={{ margin: "10px auto", textAlign: "center", color: 'black', fontSize: 'clamp(26px, 3vw, 30px)', fontFamily: 'PlaydairDisplay', fontWeight: '700' }}>Contact Me</p>
-                    <div className="FormGroup">
-                        <input type="text" id="name" name="name" placeholder={smallScreen ? "Name" : "Enter your name"} required />
-                    </div>
-                    <div className="FormGroup">
-                        <input type="email" id="email" name="email" placeholder={smallScreen ? "Email" : "Enter your email"} required />
-                    </div>
-                    <div className="FormGroup">
-                        <textarea id="message" name="message" rows="4" placeholder={smallScreen ? "Message" : "Enter your message"} required style={{ minHeight: '10vh' }}></textarea>
-                    </div>
-                    <div className="FormGroup last-form-group" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <button className='Button' type="submit"><div className='Label' style={{ fontFamily: 'PlayfairDisplay' }}>Submit</div></button>
-                        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-                        <a href="https://www.instagram.com/theebonijoi/" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-instagram" style={{ fontSize: '20px', color: 'black', marginRight: '10px' }}></i>
-                        </a>
-                        <a href="https://www.tiktok.com/@TheEboniJoi" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-tiktok" style={{ fontSize: '20px', color: 'black' }}></i>
-                        </a>
-                        </div>
-                    </div>
-                    </form>
-                    {/* {contactResponse && <p style={{display:'absolute',zIndex:'2'}}>{contactResponse}</p>} */}
-
+                        <form className="ContactForm" style={{ width: '70%', minHeight: '30vh', border: 'solid gray', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }} onSubmit={handleSubmit}>
+                            <p className='contact-title' style={{ margin: "10px auto", textAlign: "center", color: 'black', fontSize: 'clamp(26px, 3vw, 30px)', fontFamily: 'PlaydairDisplay', fontWeight: '700' }}>Contact Me</p>
+                            <div className="FormGroup">
+                                <input type="text" id="name" name="name" placeholder={smallScreen ? "Name" : "Enter your name"} required />
+                            </div>
+                            <div className="FormGroup">
+                                <input type="email" id="email" name="email" placeholder={smallScreen ? "Email" : "Enter your email"} required />
+                            </div>
+                            <div className="FormGroup">
+                                <textarea id="message" name="message" rows="4" placeholder={smallScreen ? "Message" : "Enter your message"} required style={{ minHeight: '10vh' }}></textarea>
+                            </div>
+                            <div className="FormGroup last-form-group" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <button className='Button' type="submit"><div className='Label' style={{ fontFamily: 'PlayfairDisplay' }}>Submit</div></button>
+                                <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+                                    <a href="https://www.instagram.com/theebonijoi/" target="_blank" rel="noopener noreferrer">
+                                        <i className="fab fa-instagram" style={{ fontSize: '20px', color: 'black', marginRight: '10px' }}></i>
+                                    </a>
+                                    <a href="https://www.tiktok.com/@TheEboniJoi" target="_blank" rel="noopener noreferrer">
+                                        <i className="fab fa-tiktok" style={{ fontSize: '20px', color: 'black' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </FadeInSection>
             )}
@@ -161,7 +156,7 @@ function Contact() {
                     right: '3%',
                     top: '12%',
                     zIndex: 1,
-                    backgroundColor: 'black',
+                    backgroundColor: 'red',
                     color: 'white',
                     padding: '10px 10px',
                     marginRight: '20px',
@@ -174,68 +169,145 @@ function Contact() {
                 Subscribe
             </button>
             {modalOpen && (
-                <div className="modal" style={{
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
+    <div className="modal" style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 10
+    }}>
+        <div className="modal-content" ref={modalRef} style={{
+            background: `url('./DrJoiTwists.jpg')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'end',
+            padding: '20px',
+            borderRadius: '10px',
+            width: '90%',
+            maxWidth: '500px',
+            height: '80vh',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)',
+            alignItems: 'center',
+            position: 'relative',
+        }}>
+            <h2 style={{
+                background: 'rgba(255, 255, 255, 0.9)',
+                borderRadius: '5px',
+                padding: '10px 15px',
+                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                margin: '0 0 15px',
+                fontWeight: 'bold',
+                fontSize: 'clamp(16px, 4vw, 24px)',
+                textAlign: 'center',
+                width:'100%'
+            }}>Subscribe to my Newsletter</h2>
+            <p style={{
+                background: 'rgba(255, 255, 255, 0.9)',
+                borderRadius: '5px',
+                padding: '10px 15px',
+                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                fontSize: 'clamp(12px, 2vw, 16px)',
+                textAlign: 'center',
+                margin: '0 0 20px'
+            }}>Stay up to date with my content focusing on mental health and wellness, my personal products, offers, and everything else Dr. Joi.</p>
+            <form onSubmit={handleSubscribe} style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '100%'
+            }}>
+                <input
+                    type="email"
+                    id="newsletter-email"
+                    placeholder="Enter your email"
+                    required
+                    style={{
+                        maxWidth: '400px',
+                        minWidth: '100%',
+                        padding: '10px',
+                        marginBottom: '15px',
+                        border: '1px solid #ccc',
+                        borderRadius: '5px',
+                        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)'
+                    }}
+                    value={subscribeEmail}
+                    onChange={(e) => setSubscribeEmail(e.target.value)}
+                />
+                <div style={{
                     width: '100%',
-                    height: '100%',
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    zIndex: 10
-                }}>
-                <div className="modal-content modal-newsletter" ref={modalRef} style={{
-                    background: `url('./DrJoiTwists.jpg')`,
-                    backgroundSize: 'cover', // Ensure the background image covers the entire area
-                    backgroundPosition: 'center',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    padding: '20px',
+                    background: 'rgba(255, 255, 255, 0.9)',
                     borderRadius: '5px',
-                    width: '80%',
-                    height: '80%',
-                    maxWidth: '40vw',
-                    maxHeight: '70vh',
-                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                    alignItems: 'center'
+                    padding: '10px',
+                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                    marginBottom: '15px'
                 }}>
-                    <div>
-                    <h2 style={{ maxWidth:'300px',textAlign: 'center',background: `rgba(255, 255, 255, 0.8)`,border:'solid 1px black',boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",marginTop: '150px',fontWeight:'bold',fontSize: "clamp(20px, 2vw, 1.5vw)",paddingLeft:'8px',paddingRight:'8px' }}>Subscribe to my Newsletter</h2>
-                    <p style={{ maxWidth:'300px',textAlign: 'center',background: `rgba(255, 255, 255, 0.8)`,border:'solid 1px black',boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)",paddingLeft:'8px',paddingRight:'8px',fontSize: "clamp(14px, 2vw, 1.5vw)" }}>Stay up to date with my content focusing on mental health and wellness, my personal products, offers and everything else Dr. Joi.</p>
-                    <form onSubmit={handleSubscribe} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                        <input
-                            type="email"
-                            id="newsletter-email"
-                            placeholder="Enter your email"
-                            required
-                            style={{ maxWidth:'300px',width: '100%', padding: '10px', marginBottom: '10px',border:'solid 1px black',boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.5)" }}
-                            value={subscribeEmail}
-                            onChange={(e) => setSubscribeEmail(e.target.value)}
-                        />
-                        <button className='Button' type="submit" style={{ cursor: 'pointer' }}>Subscribe</button>
-                    </form>
-                    <div id="response">{subscribeResponse}</div>
-                    <button className='news-close' onClick={() => setModalOpen(false)} style={{
-                        position: 'absolute',
-                        color: 'black',
-                        top: '10px',
-                        right: '10px',
-                        backgroundColor: 'transparent',
-                        border: 'none',
-                        fontSize: '20px',
-                        cursor: 'pointer'
+                    <label style={{
+                        display: 'block',
+                        marginBottom: '10px',
+                        fontSize: 'clamp(12px, 2vw, 16px)'
                     }}>
-                        &times;
-                    </button>
-                    </div>
+                        <input
+                            type="checkbox"
+                            checked={optIn}
+                            onChange={(e) => setOptIn(e.target.checked)}
+                            required
+                            style={{ marginRight: '8px' }}
+                        />
+                        {' I agree to receive emails and confirm my subscription.'}
+                    </label>
+                    <label style={{
+                        display: 'block',
+                        fontSize: 'clamp(12px, 2vw, 16px)'
+                    }}>
+                        <input
+                            type="checkbox"
+                            checked={privacyAccepted}
+                            onChange={(e) => setPrivacyAccepted(e.target.checked)}
+                            required
+                            style={{ marginRight: '8px' }}
+                        />
+                        {' I have read and accept the '}
+                        <a href="/privacy-policy" target="_blank" rel="noopener noreferrer" style={{ color: '#007BFF' }}>Privacy Policy</a>
+                    </label>
                 </div>
-                </div>
-            )}
+                <button className='Button' type="submit" style={{
+                    backgroundColor: '#007BFF',
+                    color: 'white',
+                    padding: '10px 20px',
+                    border: 'none',
+                    borderRadius: '5px',
+                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+                    cursor: 'pointer',
+                    fontSize: 'clamp(16px, 2vw, 18px)'
+                }}>Subscribe</button>
+            </form>
+            <div id="response" style={{ marginTop: '15px', color: 'black' }}>{subscribeResponse}</div>
+            <button className='news-close' onClick={() => setModalOpen(false)} style={{
+                position: 'absolute',
+                color: 'black',
+                top: '10px',
+                right: '10px',
+                backgroundColor: 'transparent',
+                border: 'none',
+                fontSize: '24px',
+                cursor: 'pointer'
+            }}>
+                &times;
+            </button>
+        </div>
+    </div>
+)}
+
         </div>
     );
 }
 
 export default Contact;
+
