bbeb1214ae6329a8eb0e6412df9d24269ce7b250
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2024-08-12 00:09:56 -0400

    Baseline 2.24
    config stripe

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ paypal.configure({
 
 app.use(express.json());
 app.use(cors());
+
+
+app.use('/well-known', (req, res, next) => {
+  if (req.url === '/apple-developer-merchantid-domain-association') {
+    res.setHeader('Content-Type', 'text/plain');
+  }
+  next();
+});
 app.use('/well-known', express.static(join(__dirname, 'well-known')));
 
 // PayPal configuration endpoint
