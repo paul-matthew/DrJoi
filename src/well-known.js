25d0dda9ca1614688f1b94d6ee8cd141f4e29e5b
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2024-08-12 01:00:36 -0400

    Baseline 2.30
    stripe config

--- src/well-known.js
+++ src/well-known.js
@@ -3,18 +3,18 @@ import React, { useEffect, useState } from 'react';
 const WellKnownContent = () => {
   const [fileContent, setFileContent] = useState('');
 
-  let fetchURL = "";
-  if (
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-  ) {
-    fetchURL = "http://localhost:5000/.well-known/apple-developer-merchantid-domain-association";
-  } else {
-    fetchURL = "https://drjoiserver-106ea7a60e39.herokuapp.com/.well-known/apple-developer-merchantid-domain-association";
-  }
-
   useEffect(() => {
-    // Fetch the file from the .well-known directory
+    // Define fetchURL based on environment
+    let fetchURL = "";
+    if (
+      window.location.hostname === "localhost" ||
+      window.location.hostname === "127.0.0.1"
+    ) {
+      fetchURL = "http://localhost:5000/.well-known/apple-developer-merchantid-domain-association";
+    } else {
+      fetchURL = "https://drjoiserver-106ea7a60e39.herokuapp.com/.well-known/apple-developer-merchantid-domain-association";
+    }
+
     fetch(fetchURL)
       .then(response => {
         if (response.ok) {
@@ -25,14 +25,27 @@ const WellKnownContent = () => {
       })
       .then(data => setFileContent(data))
       .catch(error => console.error('Error fetching file:', error));
-  }, [fetchURL]);
+  }, []);
+
+  const htmlContent = `
+    <html>
+      <head>
+        <meta name="color-scheme" content="light dark">
+      </head>
+      <body>
+        <pre style="word-wrap: break-word; white-space: pre-wrap;">${fileContent}</pre>
+      </body>
+    </html>
+  `;
 
   return (
-    <div className="WellKnownContent">
-      <pre>{fileContent}</pre> {/* Display file content */}
-    </div>
+    <div
+      dangerouslySetInnerHTML={{ __html: htmlContent }}
+      style={{ width: '100%', height: '100vh' }} // Ensure it takes up full viewport height
+    />
   );
 };
 
 export default WellKnownContent;
+
 
